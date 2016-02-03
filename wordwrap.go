8b5b23178d609c3ee57b704c818bbdc42dc4f07f
Jesse Donat <donatj@gmail.com>
2016-02-03 16:59:02 -0600

    Removes an import and adds join

--- wordwrap.go
+++ wordwrap.go
@@ -3,7 +3,6 @@
 package wordwrap
 
 import (
-	"strings"
 	"unicode"
 	"unicode/utf8"
 )
@@ -69,5 +68,33 @@ func SplitString(s string, byteLimit uint) []string {
 
 // WrapString splits a string as with SplitString and joins together with a \n
 func WrapString(s string, byteLimit uint) string {
-	return strings.Join(SplitString(s, byteLimit), "\n")
+	return join(SplitString(s, byteLimit), "\n")
+}
+
+// Copied from https://github.com/golang/go/blob/91911e39/src/strings/strings.go#L351-L370 to avoid a large import tree
+//
+// Copyright 2009 The Go Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+//
+// Join concatenates the elements of a to create a single string.   The separator string
+// sep is placed between elements in the resulting string.
+func join(a []string, sep string) string {
+	if len(a) == 0 {
+		return ""
+	}
+	if len(a) == 1 {
+		return a[0]
+	}
+	n := len(sep) * (len(a) - 1)
+	for i := 0; i < len(a); i++ {
+		n += len(a[i])
+	}
+
+	b := make([]byte, n)
+	bp := copy(b, a[0])
+	for _, s := range a[1:] {
+		bp += copy(b[bp:], sep)
+		bp += copy(b[bp:], s)
+	}
+	return string(b)
 }
